31d058524ca263da4bb1423e5443e2dfb89eb9cb
Thomas Ngo <tngo0508@gmai.com>
2020-06-09 11:13:44 -0700

    first commit

---
+++ client/HelloWorld.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { hot } from "react-hot-loader";
+const HelloWorld = () => {
+  return (
+    <div>
+      {" "}
+      <h1>hello</h1>{" "}
+    </div>
+  );
+};
+export default hot(module)(HelloWorld);
